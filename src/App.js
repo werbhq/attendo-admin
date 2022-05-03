aeb379617406d907fcf50b1447ce0993c0a53484
M Aswin Kishore <60577077+mak626@users.noreply.github.com>
2022-05-04 00:39:38 +0530

    !temp: remove subject

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import { Admin, Resource } from "react-admin";
 
 import Classroom from "./pages/classroom/Index";
 import DashBoard from "./pages/dashboard/Dashboard";
-import Subject from "./pages/subjects";
 import { dataProvider } from "./provider/firebase";
 
 const App = () => (
@@ -13,9 +12,7 @@ const App = () => (
     dashboard={DashBoard}
   >
     <Resource {...Classroom} />
-    <Resource {...Subject} />
   </Admin>
 );
 
 export default App;
-
