ebc35996c786dedbd2e38739b004903f58055397
M Aswin Kishore <60577077+mak626@users.noreply.github.com>
2022-05-20 12:01:05 +0530

    refact: semester models, add totalSemesters param

--- src/data/models/semester.d.ts
+++ src/data/models/semester.d.ts
@@ -1,11 +1,12 @@
 export interface Semester {
-    course: 'BTECH' | 'MTECH';
-    batch: {
-        id: number;
-        sem: number | null;
-    }[];
+  course: "BTECH" | "MTECH";
+  totalSemesters: number;
+  batch: {
+    id: number;
+    sem: number | null;
+  }[];
 }
 
 export interface Courses {
-    courses: { BTECH: Semester; MTECH: Semester };
+  courses: { BTECH: Semester; MTECH: Semester };
 }
