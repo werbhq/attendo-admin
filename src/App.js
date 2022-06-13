f8b1ba4846e7d8d4e123f83eb0f846b7fac613e3
M Aswin Kishore <60577077+mak626@users.noreply.github.com>
2022-06-13 20:47:33 +0530

    style: add theme and poppins font

--- src/App.js
+++ src/App.js
@@ -1,16 +1,37 @@
 import * as React from "react";
-import { Admin, Resource } from "react-admin";
+import { Admin, defaultTheme, Resource } from "react-admin";
+import red from "@mui/material/colors/red";
+import teal from "@mui/material/colors/teal";
 
 import Classroom from "./pages/classroom/Index";
 import DashBoard from "./pages/dashboard/Dashboard";
 import Subject from "./pages/subjects/Index";
 import { authProvider, dataProvider } from "./provider/firebase";
 
+const myTheme = {
+  ...defaultTheme,
+  palette: {
+    primary: {
+      main: "#7B61FF",
+    },
+    secondary: {
+      main: "#179F97",
+    },
+    error: red,
+    contrastThreshold: 3,
+    tonalOffset: 0.2,
+  },
+  typography: {
+    fontFamily: ["Poppins", "sans-serif"].join(","),
+  },
+};
+
 const App = () => (
   <Admin
     title="Attendo Admin"
+    theme={myTheme}
     dataProvider={dataProvider}
-    authProvider={authProvider} 
+    authProvider={authProvider}
     dashboard={DashBoard}
   >
     <Resource {...Classroom} />
