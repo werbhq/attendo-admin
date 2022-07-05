e642e2c4b2758f7a662e2d18361c8be1e2894053
M Aswin Kishore <60577077+mak626@users.noreply.github.com>
2022-07-05 17:25:01 +0530

    add: sorter for list values

--- src/Utils/helpers.js
+++ src/Utils/helpers.js
@@ -11,3 +11,29 @@ export const getClassroomId = (data) => {
   if (data.isDerived) dataIdSet.push(data.subjectId);
   return dataIdSet.join("-").toUpperCase();
 };
+
+/**
+ * Sorter for getList data
+ * @param {Object} params
+ * @param {Array} data
+ */
+export const sorter = (params, data) => {
+  const sort = params?.sort;
+
+  if (sort) {
+    const field = sort.field;
+
+    data = data.sort((a, b) => {
+      if (typeof a[field] === "string") {
+        if (sort.order === "DESC") return b[field].localeCompare(a[field]);
+        return a[field].localeCompare(b[field]);
+      } else if (typeof a[field] === "number") {
+        if (sort.order === "DESC") return b[field] - a[field];
+        return a[field] - b[field];
+      }
+      return a - b;
+    });
+  }
+
+  return data;
+};
