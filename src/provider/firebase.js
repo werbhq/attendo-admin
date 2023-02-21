86ce14b563f7f6bb63f78157de19498400e2d630
amaljoe <parvathysdhanya@gmail.com>
2023-02-21 15:34:05 +0530

    prod

--- src/provider/firebase.js
+++ src/provider/firebase.js
@@ -11,7 +11,7 @@ export const FieldValue = firebase.firestore.FieldValue;
 export const FieldPath = firebase.firestore.FieldPath;
 
 const options = {};
-const kMode = "emulate"; // dev, prod, emulate
+const kMode = "prod"; // dev, prod, emulate
 
 export const dataProviderLegacy =
   kMode === "prod"
