e469406509b3d1d2f28c2fa9cc702f97e12143a3
M Aswin Kishore <60577077+mak626@users.noreply.github.com>
2023-02-21 18:01:53 +0530

    enable: functions emulator

--- src/provider/firebase.js
+++ src/provider/firebase.js
@@ -28,8 +28,8 @@ export const cloudFunctions = getFunctions();
 
 if (kMode === "emulate" && process.env.NODE_ENV !== "production") {
   firebase.firestore().useEmulator("localhost", 8090);
+  firebase.functions().httpsCallable("localhost", 5001);
   firebase.auth().useEmulator("http://localhost:9099/");
-  //firebase.functions().useEmulator("localhost", 5001);
   //firebase.storage().useEmulator("localhost", 9199);
 }
 
