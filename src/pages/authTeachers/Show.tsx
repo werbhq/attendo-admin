d85f8595621ae778bb8c336ddb4537acec180414
Sera Mariam George <leelasera19@gmail.com>
2023-03-26 01:57:31 +0530

    Classrooms of teachers are made visible

--- src/pages/authTeachers/Show.tsx
+++ src/pages/authTeachers/Show.tsx
@@ -7,17 +7,38 @@ import {
     useNotify,
     useRefresh,
     WithRecord,
+    useShowController,
+    FunctionField,
+    ReferenceArrayField,
 } from 'react-admin';
 import { AuthTeachersProviderExtended } from 'provider/custom/authorizedTeachers';
 import LoadingButton from '@mui/lab/LoadingButton';
-import { useState } from 'react';
-import { AuthorizedTeacher } from 'types/models/teacher';
+import { useState, useEffect } from 'react';
+import { AuthorizedTeacher, Teacher, TeacherClassroom } from 'types/models/teacher';
+import { dataProvider } from 'provider/firebase';
+import { MAPPING } from 'provider/mapping';
+import { Chip } from '@mui/material';
+import SK from 'pages/source-keys';
 
 const AuthorizedTeacherShow = () => {
     const notify = useNotify();
     const refresh = useRefresh();
+    const record = useShowController().record as AuthorizedTeacher;
     const [loading, setLoading] = useState(false);
+    const [classroomData, setClassroomData] = useState<TeacherClassroom[]>([]);
 
+    const fetchData = () => {
+        dataProvider.getOne<Teacher>(MAPPING.TEACHERS, { id: record.id }).then((e) => {
+            setClassroomData(Object.values(e.data.classrooms));
+        });
+    };
+
+    useEffect(() => {
+        setLoading(true);
+        fetchData();
+        setLoading(false);
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
     const handleCreation = async (record: AuthorizedTeacher) => {
         setLoading(true);
         try {
@@ -34,29 +55,80 @@ const AuthorizedTeacherShow = () => {
 
     return (
         <Show>
-            <SimpleShowLayout>
-                <TextField source="id" />
-                <EmailField source="email" />
-                <TextField source="userName" label="Name" />
-                <BooleanField source="created" looseValue />
-                <TextField source="branch" />
-                <WithRecord
-                    render={(record: AuthorizedTeacher) =>
-                        !record?.created ? (
-                            <LoadingButton
-                                variant="contained"
-                                color="primary"
-                                loading={loading}
-                                onClick={() => handleCreation(record)}
-                            >
-                                Create Account
-                            </LoadingButton>
-                        ) : (
-                            <></>
-                        )
-                    }
-                />
-            </SimpleShowLayout>
+            {loading ? (
+                <>Loading...</>
+            ) : (
+                <SimpleShowLayout>
+                    <TextField source={SK.AUTH_TEACHERS("id")} />
+                    <EmailField source={SK.AUTH_TEACHERS("email")} />
+                    <TextField source={SK.AUTH_TEACHERS("userName")} label="Name" />
+                    <BooleanField source={SK.AUTH_TEACHERS("created")} looseValue />
+                    <TextField source={SK.AUTH_TEACHERS("branch" )}/>
+                    
+                        <FunctionField
+                            label="Classroom"
+                            emptyText="-"
+                            render={() => (
+                                <ul style={{ padding: 0, margin: 0 }}>
+                                    {classroomData
+                                        .filter(
+                                            (classroom, index, self) =>
+                                                index ===
+                                                self.findIndex(
+                                                    (c) => c.classroom.id === classroom.classroom.id
+                                                )
+                                        )
+                                        .map((e) => (
+                                            <Chip                                                                                     
+                                                key={e.classroom.id}
+                                                sx={{ ml: 0.5, mt: 1 }}
+                                                label={e.classroom.id}
+                                            />
+                                        ))}
+                                </ul>
+                            )}
+                        />
+                    <FunctionField
+                        label="Subject-Classroom"
+                        emptyText='-'
+                        render={() => (
+                            <ul style={{ padding: 0, margin: 0 }}>
+                                {classroomData
+                                    .filter(
+                                        (classroom, index, self) =>
+                                            index ===
+                                            self.findIndex(
+                                                (c) => c.id === classroom.id
+                                            )
+                                    )
+                                    .map((e) => (
+                                        <Chip
+                                            key={e.id}
+                                            sx={{ ml: 0.5, mt: 1 }}
+                                            label={e.id}
+                                        />
+                                    ))}
+                            </ul>
+                        )}
+                    />
+                    <WithRecord
+                        render={(record: AuthorizedTeacher) =>
+                            !record?.created ? (
+                                <LoadingButton
+                                    variant="contained"
+                                    color="primary"
+                                    loading={loading}
+                                    onClick={() => handleCreation(record)}
+                                >
+                                    Create Account
+                                </LoadingButton>
+                            ) : (
+                                <></>
+                            )
+                        }
+                    />
+                </SimpleShowLayout>
+            )}
         </Show>
     );
 };
